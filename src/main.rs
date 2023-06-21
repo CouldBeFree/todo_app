cc6173e8e86bded54822b910227a44eebffd1e52
couldbefree <zoriaalexandr@gmail.com>
2023-06-21 14:46:21 +0300

    create todo via actix

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,17 @@
-mod state;
+use actix_web::{App, HttpServer};
+mod views;
 mod to_do;
+mod state;
 mod processes;
-use std::env;
-use serde_json::value::Value;
-use serde_json::Map;
-use state::read_file;
-use to_do::to_do_factory;
-use to_do::enums::TaskStatus;
-use processes::process_input;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command: &String = &args[1];
-    let title: &String = &args[2];
-    let state: Map<String, Value> = read_file("./state.json");
-    let status: String;
-    
-    match &state.get(*&title) {
-        Some(result) => {
-            status = result.to_string().replace('\"', "");
-        }
-        None=> {
-            status = "pending".to_owned();
-        } 
-    }
-    let item = to_do_factory(title, TaskStatus::from_string(status.to_uppercase()));
-    process_input(item, command.to_string(), &state);
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new().configure(views::views_factory);
+        return app
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
