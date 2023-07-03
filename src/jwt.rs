844beb2b058bb6f9aa0af4edc1241dc9c9c0e052
couldbefree <zoriaalexandr@gmail.com>
2023-07-03 16:21:46 +0300

    added jwt struct

---
+++ src/jwt.rs
@@ -0,0 +1,29 @@
+use actix_web::dev::Payload;
+use actix_web::{Error, FromRequest, HttpRequest};
+use futures::future::{Ready, ok};
+
+pub struct JwtToken {
+    pub message: String
+}
+
+impl FromRequest for JwtToken {
+    type Error = Error;
+    type Future = Ready<Result<JwtToken, Error>>;
+
+    fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
+        match req.headers().get("token") {
+            Some(data) => {
+                let token = JwtToken{
+                    message: data.to_str().unwrap().to_string()
+                };
+                ok(token)
+            },
+            None => {
+                let token = JwtToken{
+                    message: String::from("nothing found")
+                };
+                ok(token)
+            }
+        }
+    }
+}
