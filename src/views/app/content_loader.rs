897214bbb22299bf88edd3214f06cbe61784e2ec
couldbefree <zoriaalexandr@gmail.com>
2023-07-04 09:46:16 +0300

    added js to app

--- src/views/app/content_loader.rs
+++ src/views/app/content_loader.rs
@@ -1,9 +1,6 @@
 use std::fs;
 
 pub fn read_file(file_path: &str) -> String {
-    println!("-------------------------");
-    println!("file_path, {}", file_path.to_owned());
-    println!("-------------------------");
     let data: String = fs::read_to_string(file_path).expect(&format!("Unable to read file: {}", file_path.to_owned()));
     return data
 }
