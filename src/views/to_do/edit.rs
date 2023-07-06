90e8c15cbca6509a72c6aaa255281246af0e2916
couldbefree <zoriaalexandr@gmail.com>
2023-07-06 16:36:04 +0300

    implemented db connection pool

--- src/views/to_do/edit.rs
+++ src/views/to_do/edit.rs
@@ -6,15 +6,15 @@ use crate::json_serialization::{to_do_item::ToDoItem, to_do_items::ToDoItems};
 use crate::jwt::JwtToken;
 use crate::database::establish_connection;
 use crate::schema::to_do_table;
+use crate::database::DB;
 
-pub async fn edit(to_do_item: web::Json<ToDoItem>, token: JwtToken) -> HttpResponse {
-    let connection = establish_connection();
+pub async fn edit(to_do_item: web::Json<ToDoItem>, token: JwtToken, db: DB) -> HttpResponse {
     let results = to_do_table::table
         .filter(to_do_table::columns::title.eq(&to_do_item.title));
     
     let _ = diesel::update(results)
         .set(to_do_table::columns::status.eq("DONE"))
-        .execute(&connection);
+        .execute(&db.connection);
     
     return HttpResponse::Ok().json(ToDoItems::get_state())
 }
